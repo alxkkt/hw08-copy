1c88406c632d9839c4365a6e86e5340b9ae61051
Oleksii Kikot <oleksii.kikot@yahoo.com>
2022-04-18 17:18:42 +0200

    completed third task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,14 +1,14 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-const TIME_INDICATOR = 'videoplayer-current-time';
+const CURRENT_TIME = 'videoplayer-current-time';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
 player.on('timeupdate', throttle(saveTimeInterval, 1000));
-player.setCurrentTime(localStorage.getItem(TIME_INDICATOR));
+player.setCurrentTime(localStorage.getItem(CURRENT_TIME));
 
 function saveTimeInterval({ seconds }) {
-  localStorage.setItem(TIME_INDICATOR, seconds);
+  localStorage.setItem(CURRENT_TIME, seconds);
 }
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import throttle from 'lodash.throttle';
 
+const FEEDBACK_DATA = 'feedback-form-state';
+
+const form = document.querySelector('.feedback-form');
+
+form.addEventListener('input', throttle(onFormInput, 500));
+form.addEventListener('submit', onFormSubmit);
+
+populateTextArea();
+
+function onFormInput(e) {
+  localStorage.setItem(
+    FEEDBACK_DATA,
+    JSON.stringify({
+      email: e.target.form.elements.email.value,
+      message: e.target.form.elements.message.value,
+    }),
+  );
+}
+
+function onFormSubmit(e) {
+  e.preventDefault();
+
+  console.log(JSON.parse(localStorage.getItem(FEEDBACK_DATA)));
+
+  e.currentTarget.reset();
+  localStorage.removeItem(FEEDBACK_DATA);
+}
+
+function populateTextArea() {
+  if (localStorage.getItem(FEEDBACK_DATA)) {
+    const feedbackData = JSON.parse(localStorage.getItem(FEEDBACK_DATA));
+
+    form.elements.email.value = feedbackData.email;
+    form.elements.message.value = feedbackData.message;
+  }
+}
