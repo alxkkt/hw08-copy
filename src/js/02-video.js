f0dc6642738c6d3bdfeb6d6fc8abbd2ca44161b2
Oleksii Kikot <oleksii.kikot@yahoo.com>
2022-04-19 23:01:57 +0200

    small fixes

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -6,9 +6,9 @@ const CURRENT_TIME = 'videoplayer-current-time';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-player.on('timeupdate', throttle(saveTimeInterval, 1000));
+player.on('timeupdate', throttle(saveCurrentTime, 1000));
 player.setCurrentTime(localStorage.getItem(CURRENT_TIME));
 
-function saveTimeInterval({ seconds }) {
+function saveCurrentTime({ seconds }) {
   localStorage.setItem(CURRENT_TIME, seconds);
 }
